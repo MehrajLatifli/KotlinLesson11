629aa09a9c801da497f272a685389222b61cdc44
MehrajLatifli <meracletifli@gmail.com>
2024-04-17 01:32:12 +0400

    GOD bless universes

--- src/main/kotlin/Models/Dog.kt
+++ src/main/kotlin/Models/Dog.kt
@@ -1,6 +1,6 @@
 package Models
 
-class Dog(var isWild:Boolean?=null, var name_1:String, var weight_2:Double, var length_2:Double, var parentArrayList_2: ArrayList<Animal>?):
+data class Dog(var isWild:Boolean?=null, var name_1:String, var weight_2:Double, var length_2:Double, var parentArrayList_2: ArrayList<Animal>?):
     Animal(name_1,weight_2,length_2,parentArrayList_2) {
 
      fun Friendly(friendly:Boolean?=null):String{
